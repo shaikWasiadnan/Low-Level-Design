e7b1363da0372c9666d01cf45cada87f92ab43bd
Adnan <skadnan947@gmail.com>
2026-01-02 21:43:49 +0530

    Implemented Polymorphism

---
+++ src/main/java/LLD/Polymorphism/Shape.java
@@ -0,0 +1,5 @@
+package LLD.Polymorphism;
+
+public abstract class Shape {
+    public abstract double calculateArea();
+}
--- src/main/java/LLD/main.java
+++ src/main/java/LLD/main.java
@@ -4,6 +4,12 @@ import LLD.Encapsulation.BankAccount;
 import LLD.Inheritance.Car;
 import LLD.Inheritance.Engine;
 import LLD.Inheritance.Wheels;
+import LLD.Polymorphism.Circle;
+import LLD.Polymorphism.Rectangle;
+import LLD.Polymorphism.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class main{
     public static void main(String[] args) {
@@ -19,9 +25,15 @@ public class main{
 //        account.withdraw(500,"1230");
 //        account.deposit(1000);
 //        account.withdraw(500,"1230");
-        Engine eng = new Engine();
-        Wheels wheel = new Wheels();
-        Car car = new Car(eng,wheel);
-        car.drive();
+//        Engine eng = new Engine();
+//        Wheels wheel = new Wheels();
+//        Car car = new Car(eng,wheel);
+//        car.drive();
+        List<Shape> list = new ArrayList<>();
+        list.add(new Circle(3.0));
+        list.add(new Rectangle(3.0,4.0));
+        for(Shape item : list){
+            System.out.println(item.calculateArea());
+        }
     }
 }
